67c5839904b03c94cf85e443aa7f6c1a61d652bf
Matias <lic.matiaspuig@gmail.com>
2020-08-17 19:25:18 -0300

    Added /src example

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ *  TS EXAMPLE.
+ *  This is a template file for a simple ts.
+ */
+
+/**
+ * We are using typescript.
+ */
+const getGreeting = (name: string): string => {
+  return `Hello ${name}`;
+};
+
+export = getGreeting;
